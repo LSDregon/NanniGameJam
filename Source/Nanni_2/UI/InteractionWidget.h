e95a4c9518c3ee4dbe56c74f1275ddf29dc2d596
LSMat <alerufus96@gmail.com>
2022-05-16 18:09:01 +0200

    Game Menu V0

---
+++ Source/Nanni_2/UI/InteractionWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InteractionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom))
+class NANNI_2_API UInteractionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	//UFUNCTION(BlueprintCallable, Category = "Interaction")
+	//	void UpdateInteractionWidget(class UInteractionComponent* InteractionComponent);
+
+	//UFUNCTION(BlueprintImplementableEvent)
+	//	void OnUpdateInteractionWidget();
+
+	//UPROPERTY(BlueprintReadOnly, Category = "Interaction", meta = (ExposeOnSpawn))
+	//	UInteractionComponent* OwningInteractionComponent;
+	
+};
---
+++ Source/Nanni_2/UI/MainUserInterfaceWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainUserInterfaceWidget.generated.h"
+
+//class UDialogBoxWidget;
+/**
+ * 
+ */
+UCLASS()
+class NANNI_2_API UMainUserInterfaceWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	//	UDialogBoxWidget* DialogBox;
+	//
+	//UFUNCTION(BlueprintCallable, Category = "Dialog Box")
+	//	UDialogBoxWidget* GetDialogBox() const { return DialogBox; }
+};
